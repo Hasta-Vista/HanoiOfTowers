c75b22c5091fd61da9160c24090d4bd5658d8324
Hasta-Vista <ovchinnikov675@yandex.com>
2024-07-24 17:47:52 +0400

    vector

--- HanoiOfTowers123.cpp
+++ HanoiOfTowers123.cpp
@@ -1,44 +1,31 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
-
-static void HanoiOfTowers(int disks, int first, int third, int second, int* counter, int** massiv, int &size) {
-	if (disks != 0) {
-		HanoiOfTowers(disks - 1, first, second, third, counter, massiv, size);
-		massiv[*counter][0] = first;
-		massiv[*counter][1] = third;
-		(*counter)++;
-		if (*counter == size + 1) {
-			massiv = (int**)realloc(massiv, (size + 10) * sizeof(int));
-			size = size + 10;
-			for (int i = size - 10 ; i != size ; ++i) {
-				massiv[i] = (int*)malloc(sizeof(int) * 2);
-			}
-		}
-
-		HanoiOfTowers(disks - 1, second, third, first, counter, massiv, size);
-		
-	}
+static void HanoiOfTowers(int disks, int first, int third, int second, int* counter, vector<pair<int, int>>& moves) {
+    if (disks != 0) {
+        HanoiOfTowers(disks - 1, first, second, third, counter, moves);
+        moves.push_back({ first, third });
+        (*counter)++;
+        HanoiOfTowers(disks - 1, second, third, first, counter, moves);
+    }
 }
+
 int main()
 {
-	int size = 10;
-	int counter = 0;
-	int** massiv = (int**)malloc(sizeof(int) * size);
-	
-	for (int i = 0; i != size; ++i) {
-		massiv[i] = (int*)malloc (sizeof(int) * 2);
-	}
-	
-	int disks;
-	cin >> disks;
-	const int peg1 = 1, peg2 = 2, peg3 = 3;
-	HanoiOfTowers(disks, peg1, peg3, peg2, &counter, massiv, size);
-	cout << counter << endl;
-	for (int i = 0; i != counter; ++i) {
-		cout << massiv[i][0] << " " << massiv[i][1] << endl;
-	}
+    int counter = 0;
+    vector<pair<int, int>> moves;
 
-	return 0;
+    int disks;
+    cin >> disks;
+    const int peg1 = 1, peg2 = 2, peg3 = 3;
+    HanoiOfTowers(disks, peg1, peg3, peg2, &counter, moves);
+
+    cout << counter << endl;
+    for (const auto& move : moves) {
+        cout << move.first << " " << move.second << endl;
+    }
+
+    return 0;
 }
