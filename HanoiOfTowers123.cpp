6e058dd80af79116110d2b96e4fb8c54023d4900
Hasta-Vista <ovchinnikov675@yandex.com>
2024-07-24 18:44:07 +0400

    malloc realloc

--- HanoiOfTowers123.cpp
+++ HanoiOfTowers123.cpp
@@ -1,31 +1,48 @@
 ﻿#include <iostream>
-#include <vector>
-
 using namespace std;
 
-static void HanoiOfTowers(int disks, int first, int third, int second, int* counter, vector<pair<int, int>>& moves) {
+static void HanoiOfTowers(int disks, int first, int third, int second, int* counter, int**& massiv, int& size) {
     if (disks != 0) {
-        HanoiOfTowers(disks - 1, first, second, third, counter, moves);
-        moves.push_back({ first, third });
+        HanoiOfTowers(disks - 1, first, second, third, counter, massiv, size);
+
+        massiv[*counter][0] = first;
+        massiv[*counter][1] = third;
         (*counter)++;
-        HanoiOfTowers(disks - 1, second, third, first, counter, moves);
+
+        if (*counter == size) {
+            massiv = (int**)realloc(massiv, (size + 10) * sizeof(int*));
+            for (int i = size; i < size + 10; ++i) {
+                massiv[i] = (int*)malloc(2 * sizeof(int));
+            }
+            size += 10;
+        }
+        HanoiOfTowers(disks - 1, second, third, first, counter, massiv, size);
     }
 }
 
-int main()
-{
+int main() {
+    int size = 10;
     int counter = 0;
-    vector<pair<int, int>> moves;
+    int** massiv = (int**)malloc(sizeof(int*) * size);
+
+    for (int i = 0; i < size; ++i) {
+        massiv[i] = (int*)malloc(2 * sizeof(int));
+    }
 
     int disks;
     cin >> disks;
     const int peg1 = 1, peg2 = 2, peg3 = 3;
-    HanoiOfTowers(disks, peg1, peg3, peg2, &counter, moves);
+    HanoiOfTowers(disks, peg1, peg3, peg2, &counter, massiv, size);
 
     cout << counter << endl;
-    for (const auto& move : moves) {
-        cout << move.first << " " << move.second << endl;
+    for (int i = 0; i < counter; ++i) {
+        cout << massiv[i][0] << " " << massiv[i][1] << endl;
     }
+
+    for (int i = 0; i < size; ++i) {
+        free(massiv[i]);
+    }
+    free(massiv);
 
     return 0;
 }
